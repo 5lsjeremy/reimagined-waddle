23003ea2ceed8ed3992c0b5744d1984edfa758af
5lightdevCO <jeremy@5lightstudio.com>
2026-05-05 01:31:40 -0400

    first commit

---
+++ Astryx.Client.TestHarness/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Astryx.Client.TestHarness;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
