a4cfaeb3a28be89b01ba005d06c883e922517382
5lightdevCO <jeremy@5lightstudio.com>
2026-05-05 13:12:34 -0400

    testing systems actions and reactions

--- Astryx.Client.TestHarness/ActorFactoryTests.cs
+++ Astryx.Client.TestHarness/ActorFactoryTests.cs
@@ -1,18 +1,16 @@
 using Astryx.Abstractions;
-using Astryx.Abstractions.Agents;
-using Astryx.Abstractions.Runtime;
-using Astryx.Substrate;
+using Astryx.Abstractions.Sparks;
 using Astryx.Substrate.Factories;
-using Astryx.Substrate.Runtime;
+using Astryx.Client.TestHarness.TestSuite;
 
 namespace Astryx.Client.TestHarness
 {
     public class ActorFactoryTests
     {
         [Test]
-        public void Can_Create_Actor_From_Profile()
+        public void Factory_Assigns_Id_And_Creates_Actor()
         {
-            var identity = new Matrix(new float[,] 
+            var identity = new Matrix(new float[,]
             {
                 { 1f, 0f },
                 { 0f, 1f }
@@ -24,34 +22,97 @@ namespace Astryx.Client.TestHarness
             var actor = factory.Create(identity);
             actor.Profile.Name = "TestAgent";
 
+            Assert.That(actor.Profile.Id, Is.Not.EqualTo(Guid.Empty));
             Assert.That(actor.Profile.Name, Is.EqualTo("TestAgent"));
         }
-
     }
 
     public class RuntimeTests
     {
         [Test]
-        public void EndToEndRuntimeExecutesOneStep()
+        public void TestSimulation_Registers_Actor_And_Runs_Steps()
         {
-            var identity = new Matrix(new float[,] 
-            {
-                { 1f, 0f },
-                { 0f, 1f }
-            });
+            var runtime = new RuntimeService();
+            var sim = new TestSimulation(runtime);
 
             var resolver = new ProfileResolver();
             var factory = new AgentActorFactory(resolver);
 
-            var actor = factory.Create(identity);
+            var actor = factory.Create(new Matrix(new float[,] { { 1, 0 }, { 0, 1 } }));
             actor.Profile.Name = "TestAgent";
 
-            IRuntimeService runtime = new RuntimeService();
-            runtime.RegisterActor(actor);
+            sim.AddActor(actor);
+            sim.RunSteps(1);
 
-            runtime.Step();
+            var snapshot = sim.Snapshot(0);
 
-            Assert.That(actor.Profile.Name, Is.EqualTo("TestAgent"));
+            Assert.That(snapshot.Actors.ContainsKey(actor.Profile.Id), Is.True);
+            Assert.That(snapshot.Actors[actor.Profile.Id].Name, Is.EqualTo("TestAgent"));
+        }
+
+        [Test]
+        public void TestSimulation_Captures_Multiple_Snapshots()
+        {
+            var runtime = new RuntimeService();
+            var sim = new TestSimulation(runtime);
+
+            var resolver = new ProfileResolver();
+            var factory = new AgentActorFactory(resolver);
+
+            var actor = factory.Create(new Matrix(new float[,] { { 1, 0 }, { 0, 1 } }));
+            sim.AddActor(actor);
+
+            sim.RunSteps(3);
+
+            Assert.That(sim.Snapshot(0).StepNumber, Is.EqualTo(1));
+            Assert.That(sim.Snapshot(1).StepNumber, Is.EqualTo(2));
+            Assert.That(sim.Snapshot(2).StepNumber, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void TestSimulation_Routes_Sparks_Through_Runtime()
+        {
+            var runtime = new RuntimeService();
+            var sim = new TestSimulation(runtime);
+
+            var resolver = new ProfileResolver();
+            var factory = new AgentActorFactory(resolver);
+
+            var actor = factory.Create(new Matrix(new float[,] { { 1, 0 }, { 0, 1 } }));
+            sim.AddActor(actor);
+
+            var spark = new SparkEvent
+            {
+                SourceActorId = actor.Profile.Id,
+                TargetActorId = actor.Profile.Id,
+                Type = "test",
+                Magnitude = 1f
+            };
+
+            sim.InjectSpark(spark);
+
+            Assert.DoesNotThrow(() => sim.RunSteps(1));
+        }
+
+        [Test]
+        public void TestSimulation_Exposes_Snapshot_By_Index()
+        {
+            var runtime = new RuntimeService();
+            var sim = new TestSimulation(runtime);
+
+            var resolver = new ProfileResolver();
+            var factory = new AgentActorFactory(resolver);
+
+            var actor = factory.Create(new Matrix(new float[,] { { 1, 0 }, { 0, 1 } }));
+            sim.AddActor(actor);
+
+            sim.RunSteps(2);
+
+            var snap1 = sim.Snapshot(0);
+            var snap2 = sim.Snapshot(1);
+
+            Assert.That(snap1.StepNumber, Is.EqualTo(1));
+            Assert.That(snap2.StepNumber, Is.EqualTo(2));
         }
     }
 }
---
+++ Astryx.Client.TestHarness/TestSuite/TestSimulation.cs
@@ -0,0 +1,38 @@
+using Astryx.Abstractions.Factories;
+using Astryx.Abstractions.Runtime;
+using Astryx.Abstractions.Snapshots;
+using Astryx.Abstractions.Sparks;
+
+namespace Astryx.Client.TestHarness.TestSuite;
+
+public sealed class TestSimulation
+{
+    private readonly IRuntimeService _runtime;
+    private readonly List<StepSnapshot> _snapshots = new();
+
+    public TestSimulation(IRuntimeService runtime)
+    {
+        _runtime = runtime;
+    }
+
+    public void AddActor(IAgentActor actor)
+    {
+        _runtime.RegisterActor(actor);
+    }
+
+    public void InjectSpark(SparkEvent spark)
+    {
+        _runtime.InjectSpark(spark);
+    }
+
+    public void RunSteps(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            var snapshot = _runtime.Step();
+            _snapshots.Add(snapshot);
+        }
+    }
+
+    public StepSnapshot Snapshot(int index) => _snapshots[index];
+}
